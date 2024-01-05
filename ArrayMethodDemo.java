554a6de8bf709c56b7f279e63d83a5ae957cca42
jflan646 <47369954+jflan646@users.noreply.github.com>
2024-01-04 22:26:12 -0500

    Update ArrayMethodDemo.java
    removed 0

--- ArrayMethodDemo.java
+++ ArrayMethodDemo.java
@@ -2,7 +2,7 @@
 public class ArrayMethodDemo {
 
 	public static void main(String[] args) {
-		int[] numbers = {1,2,3,4,5,6,7,8,9,0};
+		int[] numbers = {1,2,3,4,5,6,7,8,9,};
 		display(numbers);
 		displayReverse(numbers);
 		int sum = displaySum(numbers);
